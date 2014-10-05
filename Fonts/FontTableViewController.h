a7e73fba362f27160e2a397207dbe6cdcb7c7137
Ben Chatelain <benchatelain@gmail.com>
2014-10-04 21:09:47 -0600

    List all available font families on a table

--- Fonts/FontTableViewController.h
+++ Fonts/FontTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  FontTableViewController.h
 //  Fonts
 //
 //  Created by Ben Chatelain on 10/4/14.
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface FontTableViewController : UITableViewController
 
 @end
 
---
+++ Fonts/FontTableViewController.m
@@ -0,0 +1,60 @@
+//
+//  FontTableViewController.m
+//  Fonts
+//
+//  Created by Ben Chatelain on 10/4/14.
+//  Copyright (c) 2014 phatblat. All rights reserved.
+//
+
+#import "FontTableViewController.h"
+
+@interface FontTableViewController ()
+
+@property (strong, nonatomic) NSArray *fontFamilyNames;
+
+@end
+
+@implementation FontTableViewController
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.fontFamilyNames = [UIFont familyNames];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.fontFamilyNames.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+
+    NSString *fontFamily = self.fontFamilyNames[indexPath.row];
+    cell.textLabel.text = fontFamily;
+
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
